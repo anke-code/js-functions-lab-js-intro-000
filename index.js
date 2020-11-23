1eaa6c8a625e9b883b8af4c2b799f22b76186dd3
Anke <anke.ortmann@gmail.com>
2020-11-23 14:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name){
-  returns `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name){
-  returns `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
