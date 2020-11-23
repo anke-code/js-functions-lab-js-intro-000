bb71590f4747bc9922147914059b86d497b40b55
Anke <anke.ortmann@gmail.com>
2020-11-23 14:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   returns `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, days)
-{
+
+function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
 }
